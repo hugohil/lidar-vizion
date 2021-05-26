a6ef46294365d9706d3952dc0734354383a39575
hugohil <hilairehugo@gmail.com>
2021-05-26 20:50:55 +0200

    Add vida for blob detection

--- src/app/index.js
+++ src/app/index.js
@@ -23,11 +23,11 @@ function onLidarRegister (serial) {
     gui: createGui(serial),
     params: {
       offsetX: 0,
-      offsetXMin: -(windowWidth * .5),
-      offsetXMax: (windowWidth * .5),
+      offsetXMin: -(windowWidth * .25),
+      offsetXMax: (windowWidth * .25),
       offsetY: 0,
-      offsetYMin: -(windowHeight * .5),
-      offsetYMax: (windowHeight * .5),
+      offsetYMin: -(windowHeight * .25),
+      offsetYMax: (windowHeight * .25),
       scale: 1,
       scaleStep: 0.01,
       scaleMin: 0.01,
@@ -50,46 +50,80 @@ function onLidarData (data) {
   }
 }
 
+let vida = null
+let pg = null
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight)
+
+  pg = createGraphics((windowWidth * 0.25), (windowHeight * 0.25))
+
+  vida = new Vida(this)
+  vida.handleBlobsFlag = true
+  vida.trackBlobsFlag = true
+  vida.approximateBlobPolygonsFlag = true
+  vida.pointsPerApproximatedBlobPolygon = 5
+  vida.progressiveBackgroundFlag = true
+  vida.imageFilterFeedback = 0.9 // probably best to stay between .9 and .98
+  vida.imageFilterThreshold = 0.1
+  vida.normMinBlobArea = 0.0002
+  vida.normMaxBlobArea = 0.5
 }
 
 let maxDistance = 5000
 const rad = (Math.PI / 180)
 
 function draw() {
-  noStroke()
+  // background('rgba(0,0,0, 0.25)');
   background(0);
+
+  pg.noStroke()
+  // pg.background('rgba(0,0,0, 0.25)');
+  pg.background(0);
   
-  fill(255, 255, 255)
+  pg.fill(255, 255, 255)
   for (serial in devices) {
     let x, y = 0
 
     const d = devices[serial]
 
-    push()
-    fill(d.params.color)
-    const offsetX = (d.params.offsetX + (width * 0.5))
-    const offsetY = (d.params.offsetY + (height * 0.5))
-    translate(offsetX, offsetY)
-    rotate(d.params.angle * rad)
-    scale(d.params.scale)
+    pg.push()
+    pg.fill(d.params.color)
+    const offsetX = (d.params.offsetX + (pg.width * 0.5))
+    const offsetY = (d.params.offsetY + (pg.height * 0.5))
+    pg.translate(offsetX, offsetY)
+    pg.rotate(d.params.angle * rad)
+    pg.scale(d.params.scale)
     
     d.distances.forEach((distance, index) => {
       radians = (index * rad)
       maxDistance = Math.max(distance, maxDistance)
 
       x = (distance * cos(radians))
-      x = map(x, 0, maxDistance, 0, width)
+      x = map(x, 0, maxDistance, 0, pg.width)
 
       y = (distance * sin(radians))
-      y = map(y, 0, maxDistance, 0, height)
+      y = map(y, 0, maxDistance, 0, pg.height)
 
-      ellipse(x, y, 5, 5)
+      pg.ellipse(x, y, 5, 5)
     })
 
-    fill(255, 0, 0)
-    ellipse(0, 0, 5, 5)
-    pop()
+    pg.fill(255, 0, 0)
+    pg.ellipse(0, 0, 5, 5)
+    pg.pop()
   }
+
+  vida.update(pg)
+
+  // image(pg, 0, 0, width, height)
+  // vida.drawBlobs(0, 0, width, height)
+
+  image(pg, 0, 0, width/2, height/2)
+  image(vida.backgroundImage, width/2, 0, width/2, height/2)
+  image(vida.differenceImage, 0, height/2, width/2, height/2)
+  image(vida.thresholdImage, width/2, height/2, width/2, height/2)
+  vida.drawBlobs(width/2, height/2, width/2, height/2)
+
+  fill(0, 255, 0)
+  text(getFrameRate(), 10, 500)
 }
